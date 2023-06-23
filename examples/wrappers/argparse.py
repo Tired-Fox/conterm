00b06668f49c20d0f42bfceec3a40fae2bde0f5f
Zachary Boehm <zboehm@menard-inc.com>
2023-06-23 10:28:27 -0500

    Add examples

--- examples/wrappers/argparse.py
+++ examples/wrappers/argparse.py
@@ -1,23 +1,6 @@
-import click
+"""This example shows that you can manually run the click cli with custom cmd."""
+from cli import cli
 from conterm.wrapper import run_click
-
-@click.group()
-def cli():
-    """A test cli tool."""
-
-@cli.group("set")
-def set_env():
-    """Set program environment variables."""
-
-@set_env.command()
-def client():
-    """Set the programs client."""
-    print("set client")
-
-@set_env.command()
-def database():
-    """Set the programs database."""
-    print("set database")
 
 if __name__ == "__main__":
     output = run_click(cli, "set")
