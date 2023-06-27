cbfad457467f0aa3ea667df6e6712ce0993de7e0
Zachary Boehm <zboehm@menard-inc.com>
2023-06-27 11:42:56 -0500

    Add pretty print with themes

--- conterm/printing/__init__.py
+++ conterm/printing/__init__.py
@@ -1,1 +1,2 @@
 from .markup import *
+from .pprint import pprint
---
+++ examples/pprint.py
@@ -0,0 +1,48 @@
+from conterm.printing import pprint
+from conterm.printing.themes import DRACULA, GRUVBOX, NORD, Catpuccin
+
+class Something:
+    pass
+
+def something():
+    pass
+
+if __name__ == "__main__":
+    # Themes can also be manually created.
+    # They use a dict of the format:
+    # class Theme(TypedDict):
+    #     keyword: str
+    #     object: str
+    #     string: str
+    #     number: str
+    #     comment: str
+
+    pprint(
+        "[b]List:",
+        ["Hello", 3, 123.456, ["data"], None],
+        sep="\n",
+        end="\n\n",
+    )
+    pprint(
+        "[b]Dict:",
+        {"key": Something, "second": 3, "third": set([something, Something])},
+        sep="\n",
+        theme=NORD
+    )
+    pprint(
+        {"key": Something, "second": 3, "third": set([something, Something])},
+        sep="\n",
+        theme=DRACULA
+    )
+    pprint(
+        {"key": Something, "second": 3, "third": set([something, Something])},
+        sep="\n",
+        end="\n\n",
+        theme=GRUVBOX
+    )
+    pprint(
+        {"key": Something, "second": 3, "third": set([something, Something])},
+        sep="\n",
+        end="\n\n",
+        theme=Catpuccin.MOCHA
+    )
